8b8d2375799548951edd74ce6b338f0bac199fe6
nraj <nraj@cloudera.com>
2022-06-15 14:34:18 -0400

    nodes.py fix

--- utilities/nodes.py
+++ utilities/nodes.py
@@ -19,87 +19,81 @@ import requests
 import base64
 import getpass
 
+# Configure HTTPS authentication
+cm_client.configuration.username = 'username'
+cm_client.configuration.password = 'pwd'
+cm_client.configuration.verify_ssl = False
+cm_host = 'cm_server_url'
+# Construct base URL for API
+api_host = 'https://' + cm_host
+port = '7183'
+api_version = 'v22'
+api_url = api_host + ':' + port + '/api/' + api_version
+api_client = cm_client.ApiClient(api_url)
 
-def hosts(env, username, pwd):
-    # Configure HTTPS authentication
-    cm_client.configuration.username = username
-    cm_client.configuration.password = pwd
-    cm_client.configuration.verify_ssl = False
+# Get host API
+hosts_api_instance = cm_client.HostsResourceApi(api_client)
+cm_hosts = hosts_api_instance.read_hosts(view='FULL')
 
-    environments = {'lab': 'lab-cm', 'dev': 'dev-cm', 'pre-prod': 'pre-prod-cm', 'prod': 'cm'}
-    # Construct base URL for API
-    api_host = 'https://' + environments[env]
-    port = '7183'
-    api_version = 'v19'
-    api_url = api_host + ':' + port + '/api/' + api_version  # https://<environment>:7183/api/v19
-    api_client = cm_client.ApiClient(api_url)
+# All all services if missing
+svcs = {'hdfs': {'namenodes': 'NAMENODE', 'datanodes': 'DATANODE', 'journalnodes': 'JOURNAL',
+                 'nfs_gateway': 'NFSGATEWAY', 'hdfs_gateways': '-GATEWAY', 'failover_controllers': 'FAIL',
+                 'httpfs': 'HTTPFS', 'balancer': 'BALANCER'},
+        'hive': {'hive_metastore_server': 'METASTORE', 'hiveserver2': 'SERVER2', 'webhcat_server': 'WEBHCAT',
+                 'hive_gateway': 'GATEWAY'},
+        'hue': {'hue_load_balancer': 'LOAD_BALANCER', 'hue_server': 'SERVER', 'kerberos_ticket_renewer': 'KT'},
+        'impala': {'catalog_server': 'CATALOGSERVER', 'state_store': 'STATESTORE', 'daemons': 'IMPALAD'},
+        'oozie': {'oozie_server': 'SERVER'},
+        'sentry': {'sentry_server': 'SERVER'},
+        'solr': {'solr_server': 'SERVER', 'solr_gateway': 'GATEWAY'},
+        'spark': {'spark_gateway': '-GATEWAY'},
+        'yarn': {'job_history_server': 'JOB', 'resource_manager': 'RESOURCE', 'yarn_gateway': 'yarn-GATEWAY',
+                 'node_manager': 'NODE'},
+        'zookeeper': {'zookeeper_server': 'SERVER'}}
 
-    # Get host API
-    hosts_api_instance = cm_client.HostsResourceApi(api_client)
-    cm_hosts = hosts_api_instance.read_hosts(view='FULL')
-
-    svcs = {'hdfs': {'namenodes': 'NAMENODE', 'datanodes': 'DATANODE', 'journalnodes': 'JOURNAL',
-                     'nfs_gateway': 'NFSGATEWAY', 'hdfs_gateways': '-GATEWAY', 'failover_controllers': 'FAIL',
-                     'httpfs': 'HTTPFS', 'balancer': 'BALANCER'},
-            'hive': {'hive_metastore_server': 'METASTORE', 'hiveserver2': 'SERVER2', 'webhcat_server': 'WEBHCAT',
-                     'hive_gateway': 'GATEWAY'},
-            'hue': {'hue_load_balancer': 'LOAD_BALANCER', 'hue_server': 'SERVER', 'kerberos_ticket_renewer': 'KT'},
-            'impala': {'catalog_server': 'CATALOGSERVER', 'state_store': 'STATESTORE', 'daemons': 'IMPALAD'},
-            'oozie': {'oozie_server': 'SERVER'},
-            'sentry': {'sentry_server': 'SERVER'},
-            'solr': {'solr_server': 'SERVER', 'solr_gateway': 'GATEWAY'},
-            'spark': {'spark_gateway': '-GATEWAY'},
-            'yarn': {'job_history_server': 'JOB', 'resource_manager': 'RESOURCE', 'yarn_gateway': 'yarn-GATEWAY',
-                     'node_manager': 'NODE'},
-            'zookeeper': {'zookeeper_server': 'SERVER'}}
-
-    try:
-        if "-v" in sys.argv[4]:
-            # Get Clusters
-            cluster_api_instance = cm_client.ClustersResourceApi(api_client)
-            api_response = cluster_api_instance.read_clusters(view='SUMMARY')
-            clusters = {}
-            for cluster in api_response.items:
-                clusters[cluster.name] = cluster.display_name
-            for i in svcs:
-                for j in svcs[i]:
-                    print("\n[%s]" % j)
-                    for k in cm_hosts.items:
-                        for l in k.role_refs:
-                            if i == 'yarn':
-                                if svcs[i][j] in l.role_name and 'yarn' in l.role_name and 'spark' not in l.role_name:
-                                    print(clusters[l.cluster_name], "-", k.hostname)
-                            else:
-                                if svcs[i][j] in l.role_name and i in l.role_name:
-                                    print(clusters[l.cluster_name], "-", k.hostname)
-
-    except:
-        # IndexError
-        for i in svcs:
-            for j in svcs[i]:
-                print("\n[%s]" % j)
-                for k in cm_hosts.items:
-                    for l in k.role_refs:
-                        if i == 'yarn':
-                            if svcs[i][j] in l.role_name and 'yarn' in l.role_name and 'spark' not in l.role_name:
-                                print(k.hostname)
+try:
+    if "-v" in sys.argv[4]:
+        # Get Clusters
+        cluster_api_instance = cm_client.ClustersResourceApi(api_client)
+        api_response = cluster_api_instance.read_clusters(view='SUMMARY')
+        clusters = {}
+        for cluster in api_response.items:
+            clusters[cluster.name] = cluster.display_name
+        for outer_index in svcs:
+            for inner_index in svcs[outer_index]:
+                print("\n[%s]" % inner_index)
+                for host_item in cm_hosts.items:
+                    for role_item in host_item.role_refs:
+                        if outer_index == 'yarn':
+                            if svcs[outer_index][
+                                inner_index] in role_item.role_name and 'yarn' in role_item.role_name and 'spark' not in role_item.role_name:
+                                print(clusters[role_item.cluster_name], "-", host_item.hostname)
                         else:
-                            if svcs[i][j] in l.role_name and i in l.role_name:
-                                print(k.hostname)
-    cmservers = {'lab': 'lab_cm_url', 'dev': 'dev_cm_url', 'pre-prod': 'pre_prod_cm_url', 'prod': 'prod_cm_url'}
-    print("\n[cm_server]")
-    print(cmservers[env])
+                            if svcs[outer_index][
+                                inner_index] in role_item.role_name and outer_index in role_item.role_name:
+                                print(clusters[role_item.cluster_name], "-", host_item.hostname)
 
-    print("\n[ansible_host]")
-    print(cmservers[env])
+except:
+    # IndexError
+    for outer_index in svcs:
+        for inner_index in svcs[outer_index]:
+            print("\n[%s]" % inner_index)
+            for host_item in cm_hosts.items:
+                for role_item in host_item.role_refs:
+                    if outer_index == 'yarn':
+                        if svcs[outer_index][
+                            inner_index] in role_item.role_name and 'yarn' in role_item.role_name and 'spark' not in role_item.role_name:
+                            print(host_item.hostname)
+                    else:
+                        if svcs[outer_index][inner_index] in role_item.role_name and outer_index in role_item.role_name:
+                            print(host_item.hostname)
 
-    print("\n[all_hosts]")
-    for m in cm_hosts.items:
-        print(m.hostname)
+print("\n[cm_server]")
+print(cm_host)
 
+print("\n[ansible_host]")
+print(cm_host)
 
-f = open('/tmp/encoded.txt', "r")
-passwd = base64.b64decode(f.read()).decode("utf-8").strip()
-hosts(env="lab", username="admin", pwd=passwd)
-
-
+print("\n[all_hosts]")
+for host_instance in cm_hosts.items:
+    print(host_instance.hostname)
