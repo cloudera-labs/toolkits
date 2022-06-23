cf93118b2cc9769c98ec0e57b036e8dbbff14f48
nraj <nraj@cloudera.com>
2022-06-23 11:04:56 -0600

    Added License Headers

--- data_services-toolkit/DS_Pre-Install_Check/ftype.sh
+++ data_services-toolkit/DS_Pre-Install_Check/ftype.sh
@@ -1,3 +1,17 @@
+# Copyright 2022 Cloudera, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # remove old files
 rm -rf /tmp/ftype.txt /tmp/check_xfs.txt /tmp/ftype.txt /tmp/ftype_compare.txt
 df -T | awk '{print $1,$2,$NF}' | grep "^/dev" | awk '{print $1}' > check_xfs.txt
