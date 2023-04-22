0f1e85717c5703994d34791550078598df895927
Will Dailey <wm.k.dailey@gmail.com>
2023-04-22 11:28:50 -0700

    Editing setup_wildcard_tls.sh to improve instructions.

--- edu_toolkit/bin/setup_wildcard_tls.sh
+++ edu_toolkit/bin/setup_wildcard_tls.sh
@@ -58,17 +58,19 @@ function get_help() {
 
 cat << EOF
 SYNOPSIS
-        setup_wildcard_tls.sh -d <app_domain> -n <cluster_name> -p <certificate_prefix> [ -s self sign | -c sign csr with default CA]" 1>&2
+        setup_wildcard_tls.sh -d <app_domain> -n <cluster_name> -p <certificate_prefix> [ -c sign a csr with default CA | -s self-signed certificate]" 1>&2
 
 DESCRIPTION
-	This tool will generate a signed certs file and a private key file.
-	The tool requires the PvC ECS app_domain and a prefix name for the pem 
-	files. The tool can generate a self-signed cert and a private key. 
-	The tool can generate a certificate signed by a CA. The tool is 
-	configured to  use Cloudera Manager Auto-TLS. But the tool may be 
-	configured to use the Cloudera Deploy ca_server. When using -c option, 
-	this tool must be run on the host where Cloudera Manager or Cloudera 
-	Deploy ca_server is installed.
+	This tool will generate a signed certificate file and a private key file.
+	The certificate is signed with a Subject Alternative Name, i.e. a wildcard.
+	The command line requires the ECS apps_domain and a prefix name for the 
+	certificate files. The tool can generate a certificate signed by a CA or it can 
+	generate a self-signed certifcate. The tool is configured to use the IPA
+	CA as the root CA. A set up requirement is to extract the private key from
+	IPA and copy it into the local /etc/ipa directory. Two openssl configuration
+	files are required. The first provides configuration for the CA on the 
+	local host. The second provides configuration for the Subject Alternative
+	Name, this is where the wildcard is configured.
 
 	-h)
 		Help page
@@ -84,6 +86,7 @@ DESCRIPTION
 		Sign with self_sign
 
 EXAMPLE
+	setup_wildcard_tls.sh -d <domain: sam-lon03.example.com> -n <name of cluster: sam-lon03> -p name of key: sam> -c (certificate signed by a CA) 
 	setup_wildcard_tls.sh -d apps.edu-dev.example.com -n edu-dev -p edu-dev -s 
 	setup_wildcard_tls.sh -d apps.fin-prd.example.com -n fin-prd -p prodrisk -c 
 
