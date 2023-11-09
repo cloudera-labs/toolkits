a4e570b5186ab77d3a06df8097a1538ac15d072d
Will Dailey <wm.k.dailey@gmail.com>
2023-11-09 12:58:49 +0100

    Editing manage_hdfs_user.sh to change the useadmin to use the syntax required for Kerberos.

--- edu_toolkit/bin/manage_hdfs_user.sh
+++ edu_toolkit/bin/manage_hdfs_user.sh
@@ -27,7 +27,8 @@
 # Date: 180318
 # Purpose: Create HDFS user in support of HDP. Add a list of HDFS 
 # user from the user.txt file. Create hdfs user working directory 
-# and set quotas. 
+# and set quotas. This script expects the cluster to be using 
+# Kerberos.
 
 # DEBUG
 #set -x
@@ -38,6 +39,8 @@
 num_arg=$#
 dir=${HOME}
 option=$1
+adminuser=allan_admin
+password=BadPass@1
 file_quota=$2
 space_quota=$3
 user=${dir}/conf/list_user.txt
@@ -98,8 +101,8 @@ function add_user() {
 
 	while IFS=: read -r new_user new_group; do
 		echo "Adding working directory for ${new_user}"
-		sudo -u hdfs hdfs dfs -mkdir /user/${new_user}
-        	sudo -u hdfs hdfs dfs -chown ${new_user}:${new_group} /user/${new_user}
+		echo ${password} | su -l ${adminuser} -c "hdfs dfs -mkdir /user/${new_user}"
+        	echo ${password} | su -l ${adminuser} -c "hdfs dfs -chown ${new_user}:${new_group} /user/${new_user}"
 	done < ${user}
 }
 
@@ -109,14 +112,14 @@ function delete_user() {
 
 	while IFS=: read -r new_user new_group; do
 		echo "Deleting working directory for ${new_user} from HDFS."
-		sudo -u hdfs hdfs dfs -rm -r -skipTrash /user/${new_user}
+		echo ${password} | su -l ${adminuser} -c "hdfs dfs -rm -r -skipTrash /user/${new_user}"
 	done < ${user}
 }
 
 function list_user() {
 # List hdfs user
 
-        sudo -u hdfs hdfs dfs -ls /user
+        echo ${password} | su -l ${adminuser} -c "hdfs dfs -ls /user"
 }
 
 function set_quota() {
@@ -124,8 +127,8 @@ function set_quota() {
 
 	while IFS=: read -r new_user new_group; do
 		echo "Setting quotas for ${new_user}"
-        	sudo -u hdfs hdfs dfsadmin -setQuota ${file_quota} /user/${new_user}
-        	sudo -u hdfs hdfs dfsadmin -setSpaceQuota ${space_quota} /user/${new_user}
+        	echo ${password} | su -l ${adminuser} -c "hdfs dfsadmin -setQuota ${file_quota} /user/${new_user}"
+        	echo ${password} | su -l ${adminuser} -c "hdfs dfsadmin -setSpaceQuota ${space_quota} /user/${new_user}"
 	done < ${user}
 }
 
@@ -134,8 +137,8 @@ function clear_quota() {
 
 	while IFS=: read -r new_user new_group; do
 		echo "Clearing quotas for ${new_user}"
-        	sudo -u hdfs hdfs dfsadmin -clrQuota /user/${new_user}
-        	sudo -u hdfs hdfs dfsadmin -clrSpaceQuota /user/${new_user}
+        	echo ${password} | su -l ${adminuser} -c "hdfs dfsadmin -clrQuota /user/${new_user}"
+        	echo ${password} | su -l ${adminuser} -c "hdfs dfsadmin -clrSpaceQuota /user/${new_user}"
 	done < ${user}
 }
 
@@ -144,7 +147,7 @@ function list_quota() {
 
 	while IFS=: read -r new_user new_group; do
 		echo "Listing quotas for ${new_user}"
-        	sudo -u hdfs hadoop fs -count -q -h /user/${new_user}
+        	echo ${password} | su -l ${adminuser} -c "hdfs dfs -count -q -h /user/${new_user}"
 	done < ${user}
 }
 
